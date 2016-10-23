e00d6aa119894ccf35f02db4b9a56f409a823541
Jesse Dunietz <yavyash@gmail.com>
2016-10-23 18:14:33 -0400

    Deleted cruft code

--- parser/c2.h
+++ parser/c2.h
@@ -223,15 +223,6 @@ public:
     }
 
     actionsFile.close();
-    /*  std::string oov="oov";
-     vocab->posToInt[oov]=maxPos;
-     vocab->intToPos[maxPos]=oov;
-     npos=maxPos;
-     maxPos++;
-     wordsToInt[oov]=max;
-     intToWords[max]=oov;
-     nwords=max;
-     max++;*/
 
     std::cerr << "done." << "\n";
     for (auto a : vocab->actions) {
@@ -332,11 +323,11 @@ public:
         }
         initial = false;
       } else if (count == 1) {
-        auto actionIter = std::find(vocab->actions.begin(),
-                                    vocab->actions.end(), lineS);
-        if (actionIter != vocab->actions.end()) {
+        auto action_iter = std::find(vocab->actions.begin(),
+                                     vocab->actions.end(), lineS);
+        if (action_iter != vocab->actions.end()) {
           unsigned actionIndex = std::distance(vocab->actions.begin(),
-                                               actionIter);
+                                               action_iter);
           correct_act_sent[sentence].push_back(actionIndex);
         } else {
           // TODO: right now, new actions which haven't been observed in
@@ -366,119 +357,7 @@ public:
       pos += replace.length();
     }
   }
-
-
-/*  inline unsigned max() const { return words_.size(); }
-  inline unsigned size() const { return words_.size(); }
-  inline unsigned count(const std::string& word) const { return d_.count(word); }*/
-
-/*  static bool is_ws(char x) {
-    return (x == ' ' || x == '\t');
-  }
-
-  inline void ConvertWhitespaceDelimitedLine(const std::string& line, std::vector<unsigned>* out) {
-    size_t cur = 0;
-    size_t last = 0;
-    int state = 0;
-    out->clear();
-    while(cur < line.size()) {
-      if (is_ws(line[cur++])) {
-        if (state == 0) continue;
-        out->push_back(Convert(line.substr(last, cur - last - 1)));
-        state = 0;
-      } else {
-        if (state == 1) continue;
-        last = cur - 1;
-        state = 1;
-      }
-    }
-    if (state == 1)
-      out->push_back(Convert(line.substr(last, cur - last)));
-  }
-
-  inline unsigned Convert(const std::string& word, bool frozen = false) {
-    Map::iterator i = d_.find(word);
-    if (i == d_.end()) {
-      if (frozen)
-        return 0;
-      words_.push_back(word);
-      d_[word] = words_.size();
-      return words_.size();
-    } else {
-      return i->second;
-    }
-  }
-
-  inline const std::string& Convert(const unsigned id) const {
-    if (id == 0) return b0_;
-    return words_[id-1];
-  }
-  template<class Archive> void serialize(Archive& ar, const unsigned int version) {
-    ar & b0_;
-    ar & words_;
-    ar & d_;
-  }
- private:
-  std::string b0_;
-  std::vector<std::string> words_;
-  Map d_;*/
 };
-
-/*void ReadFromFile(const std::string& filename,
-                  Corpus* d,
-                  std::vector<std::vector<unsigned> >* src,
-                  std::set<unsigned>* src_vocab) {
-  std::cerr << "Reading from " << filename << std::endl;
-  std::ifstream in(filename);
-  assert(in);
-  std::string line;
-  int lc = 0;
-  while(getline(in, line)) {
-    ++lc;
-    src->push_back(std::vector<unsigned>());
-    d->ConvertWhitespaceDelimitedLine(line, &src->back());
-    for (unsigned i = 0; i < src->back().size(); ++i) src_vocab->insert(src->back()[i]);
-  }
-}
-
-void ReadParallelCorpusFromFile(const std::string& filename,
-                                Corpus* d,
-                                std::vector<std::vector<unsigned> >* src,
-                                std::vector<std::vector<unsigned> >* trg,
-                                std::set<unsigned>* src_vocab,
-                                std::set<unsigned>* trg_vocab) {
-  src->clear();
-  trg->clear();
-  std::cerr << "Reading from " << filename << std::endl;
-  std::ifstream in(filename);
-  assert(in);
-  std::string line;
-  int lc = 0;
-  std::vector<unsigned> v;
-  const unsigned kDELIM = d->Convert("|||");
-  while(getline(in, line)) {
-    ++lc;
-    src->push_back(std::vector<unsigned>());
-    trg->push_back(std::vector<unsigned>());
-    d->ConvertWhitespaceDelimitedLine(line, &v);
-    unsigned j = 0;
-    while(j < v.size() && v[j] != kDELIM) {
-      src->back().push_back(v[j]);
-      src_vocab->insert(v[j]);
-      ++j;
-    }
-    if (j >= v.size()) {
-      std::cerr << "Malformed input in parallel corpus: " << filename << ":" << lc << std::endl;
-      abort();
-    }
-    ++j;
-    while(j < v.size()) {
-      trg->back().push_back(v[j]);
-      trg_vocab->insert(v[j]);
-      ++j;
-    }
-  }
-}*/
 
 } // namespace
 
